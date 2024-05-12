2f45bc4439334714081e848f2278b252ba8311a2
phandinhthe <phandinhthe1991@gmail.com>
2024-05-12 23:53:51 +0700

    Apply spring cloud function

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,36 @@
 package org.example;
 
+import org.example.calculation.CalculationFactory;
+import org.example.calculation.CalculationInput;
+import org.example.calculation.CalculationOutput;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+
+	@Bean
+	Function<CalculationInput, CalculationOutput> handleRequest() {
+		System.out.println("testing calculation....");
+		return input -> {
+			System.out.println("start testing calculation....");
+			Number result = CalculationFactory
+					.calculation(input.operation()).calculate(input.number1(), input.number2());
+
+			System.out.println("having output already....");
+			System.out.println(result);
+			return new CalculationOutput(result, "Successfully calculated!", "200-OK");
+
+		};
+	}
 }
